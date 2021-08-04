7e3d9273647f770f4df72cdf36753f5f36630367
gabitheiss <t_gaaby@hotmail.com>
2021-08-04 14:26:58 -0300

    Adicionando e removendo caracteres no CPF

--- app/src/main/java/com/listas_interface/MainActivity.kt
+++ app/src/main/java/com/listas_interface/MainActivity.kt
@@ -15,9 +15,8 @@ import com.listas_interface.adapter.InterfaceClickableCarrinho
 class MainActivity : AppCompatActivity(), InterfaceClickableCarrinho {
 
     // variavel irá receber a instancia do recycler view que vem do xml
-    private lateinit var listaRecyclerView : RecyclerView
+    private lateinit var listaRecyclerView: RecyclerView
     private lateinit var produtosAdapterList: AdapterList
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,13 +25,13 @@ class MainActivity : AppCompatActivity(), InterfaceClickableCarrinho {
 
         // usamos o mutable para conseguir trabalhar com a lista
         val listOfProduct = mutableListOf<product>(
-            product("TV","R$2.000,00"),
-            product("Smarphone","R$3.000,00"),
-            product("Soundbar","R$1.500,00"),
-            product("Ar Condicionado","R$1.800,00"),
-            product("Aparelho de Som","R$1.000,00"),
+            product("TV", "R$2.000,00"),
+            product("Smarphone", "R$3.000,00"),
+            product("Soundbar", "R$1.500,00"),
+            product("Ar Condicionado", "R$1.800,00"),
+            product("Aparelho de Som", "R$1.000,00"),
 
-        )
+            )
 
         //iniciamos a instancia do adapter parrando o array como parametro
         produtosAdapterList = AdapterList(listOfProduct, this)
@@ -41,17 +40,17 @@ class MainActivity : AppCompatActivity(), InterfaceClickableCarrinho {
         //variavel listaRecyclerView está recebendo o recycler que vem do xml
         listaRecyclerView = findViewById(R.id.recyclerView)
         // define a forma que a lista irá trabalhar (GRID, LINEAR, SCROLL VERTICAL OU HORIZONTAL)
-        listaRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        listaRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         // adicionar o adapter no recycler view
         listaRecyclerView.adapter = produtosAdapterList
-
 
 
     }
 
     override fun onEdit(product: product) {
         //foi criado uma extension com o nome concatWithCompany
-     //showMessage(listaRecyclerView, produtosAdapterList.nome.concatNameWithCompany)
+        //showMessage(listaRecyclerView, produtosAdapterList.nome.concatNameWithCompany)
     }
 
     override fun onDelete(product: product) {
@@ -62,21 +61,33 @@ class MainActivity : AppCompatActivity(), InterfaceClickableCarrinho {
 
     override fun onNew(product: product) {
         if (listaRecyclerView.adapter is AdapterList) {
-            (listaRecyclerView.adapter as AdapterList).add(product("Microondas", "R$700,00",))
+            (listaRecyclerView.adapter as AdapterList).add(product("Microondas", "R$700,00"))
         }
-      }
     }
+}
 
-   fun AppCompatActivity.showMessage(view: View, message: String){
-     Snackbar.make(view, message,LENGTH_LONG).show()
+fun AppCompatActivity.showMessage(view: View, message: String) {
+    Snackbar.make(view, message, LENGTH_LONG).show()
+}
+
+
+//criando e aplicando extension
+
+
+
+fun String.removeCaracter(): String {
+    return replace(".", "").replace("-", "")
+}
+
+
+
+fun String.cpf() : String? {
+   if (length == 11){
+       return substring(0, 3) + "." +
+               substring(3,6) + "." +
+               substring(6,9) + "-" +
+               substring(9,11)
    }
-
-
-   //criando e aplicando extension
-
-   val retorno = "000.000.000-00".removeCaracter()
-
-   fun String.removeCaracter(): String {
-       return replace(".","").replace("-","")
-
-   }
+    //se o cpf não tiver 11 caracteres, vai retornar null
+    return null
+}
---
+++ app/src/main/java/com/listas_interface/Runner.kt
@@ -0,0 +1,16 @@
+package com.listas_interface
+
+fun main(){
+    val retorno = "000.000.000-00".removeCaracter()
+
+    "07814227950".cpf()
+
+    println(retorno)
+
+    println("07814227950".cpf())
+}
+
+
+
+
+
